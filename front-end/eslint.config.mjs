8350a70875bdeb3129408f3c81711b3c52928ec4
Bayzid Alim <bayzidalim@gmail.com>
2025-11-03 15:18:23 +0600

    Chore: Downgrade common ESLint/TS rules to warnings across the app

--- front-end/eslint.config.mjs
+++ front-end/eslint.config.mjs
@@ -13,7 +13,28 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn", // Change from error to warning
+      // Turn common blocking rules into warnings (do not fail CI/builds)
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
+      "@typescript-eslint/restrict-template-expressions": "warn",
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "@typescript-eslint/no-misused-promises": "warn",
+      "@typescript-eslint/await-thenable": "warn",
+      "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+      "@typescript-eslint/prefer-nullish-coalescing": "warn",
+      // General JS/React rules as warnings
+      "no-console": ["warn", { allow: ["warn", "error", "info"] }],
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "no-constant-condition": "warn",
+      "no-duplicate-imports": "warn",
+      "no-useless-escape": "warn",
+      "react-hooks/rules-of-hooks": "warn",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];
